af97b45c018487d0ab4214970d7db30a3a4ee82f
tadej <tadej@fata-mint>
2017-02-19 21:33:00 +0100

    Added test and some mdb logic.

--- message.processing.core/src/main/java/message/processing/core/MessageMDB.java
+++ message.processing.core/src/main/java/message/processing/core/MessageMDB.java
@@ -1,10 +1,17 @@
 package message.processing.core;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.ejb.ActivationConfigProperty;
 import javax.ejb.MessageDriven;
 import javax.jms.Message;
 import javax.jms.MessageListener;
 import javax.jms.ObjectMessage;
-import javax.ejb.ActivationConfigProperty;
+import javax.validation.constraints.NotNull;
 
 import message.processing.model.MultipleProductsDTO;
 import message.processing.model.ProductAdjustmentDTO;
@@ -15,18 +22,23 @@ import message.processing.model.ProductDTO;
 		@ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue"),
 		@ActivationConfigProperty(propertyName = "destination", propertyValue = "java:/jboss/exported/jms/queue/MessageProcessingQueue"),
 		@ActivationConfigProperty(propertyName = "ConnectionFactoryName", propertyValue = "ConnectionFactory"),
-		@ActivationConfigProperty(propertyName = "MaxPoolSize", propertyValue = "1"),
+		@ActivationConfigProperty(propertyName = "maxSession", propertyValue = "1"),
 		@ActivationConfigProperty(propertyName = "MaxMessages", propertyValue = "1"),
 		@ActivationConfigProperty(propertyName = "useJNDI", propertyValue = "true") })
 public class MessageMDB implements MessageListener {
 
+	private Map<String, List<MultipleProductsDTO>> productMap = new HashMap<>();
+
 	public void onMessage(Message msg) {
 		try {
+			System.out.println("ProductMap size: [" + productMap.values()== null?0:productMap.values().size() + "]");
 			ObjectMessage objMsg = (ObjectMessage) msg;
 			if (objMsg.getObject() instanceof ProductDTO) {
 				System.out.println("New ProductDTO received");
+				this.addProductToMap(new MultipleProductsDTO((ProductDTO) objMsg.getObject()));
 			} else if (objMsg.getObject() instanceof MultipleProductsDTO) {
 				System.out.println("New MultipleProductsDTO received");
+				this.addProductToMap((MultipleProductsDTO) objMsg.getObject());
 			} else if (objMsg.getObject() instanceof ProductAdjustmentDTO) {
 				System.out.println("New ProductAdjustmentDTO received");
 			} else {
@@ -34,6 +46,17 @@ public class MessageMDB implements MessageListener {
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
+		}
+
+	}
+
+	private void addProductToMap(@NotNull MultipleProductsDTO p) {
+		if (p.getProduct() != null && p.getProduct().getType() != null) {
+			if (productMap.containsKey(p.getProduct().getType().trim().toUpperCase())) {
+				productMap.get(p.getProduct().getType().trim().toUpperCase()).add(p);
+			} else {
+				productMap.put(p.getProduct().getType().trim().toUpperCase(), new ArrayList<>(Arrays.asList(p)));
+			}
 		}
 
 	}
--- message.processing.sender/src/test/java/message/processing/sender/MessageSenderTest.java
+++ message.processing.sender/src/test/java/message/processing/sender/MessageSenderTest.java
@@ -3,6 +3,7 @@ package message.processing.sender;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -25,11 +26,15 @@ public class MessageSenderTest {
 	
 	@Test
 	public void initalTest() throws Exception{
+		List<Serializable> inList = new ArrayList<Serializable>();
+		for(int i=0; i < 15; i++){
 		ProductDTO p = new ProductDTO();
 		p.setPrice(1.0f);
 		p.setPriceUnit(PriceUnit.GBP);
 		p.setType("APPLE");
-		sender.sender(new ArrayList<Serializable>(Arrays.asList(p)));
+		inList.add(p);
+		}
+		sender.sender(inList);
 	}
 	
 }
